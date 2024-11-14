e1b526a73e933e5172dddb47f175ee2123d673d7
iXZed026 <danyal.titanka24@gmail.com>
2024-11-14 19:16:34 +0330

    add background color for app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="app">
+    <div className="app app bg-gradient-to-b from-[#30142b] to-[#2772a1] h-screen">
       hello world!
     </div>
   )
