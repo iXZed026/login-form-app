cad291434002681d206356c87e0df07cc2264e83
iXZed026 <danyal.titanka24@gmail.com>
2024-11-15 21:42:21 +0330

    complete register validation and import

--- backend/src/routes/auth/index.js
+++ backend/src/routes/auth/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("./controller");
+const validate = require("./validate")
 
 router.post(
     "/register",
+    validate.registerValidator(),
     controller.register,
 );
 
