1f3304d4c2eb221502cc88a5db1ebd441f919a2b
iXZed026 <danyal.titanka24@gmail.com>
2024-11-15 16:53:03 +0330

    add post register route

---
+++ backend/src/routes/auth/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("./controller");
+
+router.post(
+    "/register",
+
+);
+
+module.exports = router;
