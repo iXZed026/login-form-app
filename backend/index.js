601fb44951bab666de8fa47a42f75f3dbaef1389
iXZed026 <danyal.titanka24@gmail.com>
2024-11-15 16:56:11 +0330

    import all route in index main project

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,15 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const config = require("config");
+const router = require("./src/routes/index")
 
 mongoose.connect(config.get("db.address"))
-.then(()=>console.log("Connect to mongoDb"))
-.catch((err)=>console.log(err))
+    .then(() => console.log("Connect to mongoDb"))
+    .catch((err) => console.log(err))
 
 app.use(express.json());
 
+app.use("/api", router)
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log("Liste to port ",port))
+app.listen(port, () => console.log("Liste to port ", port))
